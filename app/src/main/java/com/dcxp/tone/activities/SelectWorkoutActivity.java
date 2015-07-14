410616ea748829e7b216f2b427145ab51d513b1e
Daniel <dan_pc@live.com>
2015-07-14 17:31:28 -0400

    Added main activity and workout activity

--- app/src/main/java/com/dcxp/tone/activities/SelectWorkoutActivity.java
+++ app/src/main/java/com/dcxp/tone/activities/SelectWorkoutActivity.java
@@ -1,23 +1,25 @@
-package com.dcxp.tone;
+package com.dcxp.tone.activities;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.dcxp.tone.R;
 
-public class MainActivity extends ActionBarActivity {
+
+public class SelectWorkoutActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_select_workout);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_select_workout, menu);
         return true;
     }
 
