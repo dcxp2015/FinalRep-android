c557120908d3a409322ca46d69029ea8ae26061b
Daniel <dan_pc@live.com>
2015-07-17 12:24:16 -0400

    Download button reacts on click, added a progress spinner for it

--- app/src/main/java/com/dcxp/tone/uploadexplorer/UploadRVAdapter.java
+++ app/src/main/java/com/dcxp/tone/uploadexplorer/UploadRVAdapter.java
@@ -8,7 +8,9 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
 import android.widget.ImageButton;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.dcxp.tone.R;
@@ -26,10 +28,20 @@ public class UploadRVAdapter extends RecyclerView.Adapter<UploadRVAdapter.ViewHo
     }
 
     @Override
-    public void onBindViewHolder(ViewHolder holder, int position) {
-        UploadedPhrase upload = uploads.get(position);
+    public void onBindViewHolder(final ViewHolder holder, final int position) {
+        final UploadedPhrase upload = uploads.get(position);
         holder.title.setText(upload.getName());
         holder.submitter.setText(upload.getSubmitter());
+
+        // Have to set the listener here because download will delete from the recycler view and needs a position to remove from
+        holder.downloadButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Dynamically insert the progress bar in place of the download button
+                holder.downloadContainer.removeAllViews();
+                holder.downloadContainer.addView(holder.progressBar);
+            }
+        });
     }
 
     @Override
@@ -50,6 +62,8 @@ public class UploadRVAdapter extends RecyclerView.Adapter<UploadRVAdapter.ViewHo
         private ImageButton downloadButton;
         private TextView title;
         private TextView submitter;
+        private FrameLayout downloadContainer;
+        private ProgressBar progressBar;
 
         public ViewHolder(View view) {
             super(view);
@@ -59,11 +73,23 @@ public class UploadRVAdapter extends RecyclerView.Adapter<UploadRVAdapter.ViewHo
             downloadButton = (ImageButton) view.findViewById(R.id.ibtn_download);
             title = (TextView) view.findViewById(R.id.txtv_title);
             submitter = (TextView) view.findViewById(R.id.txtv_submitter);
-
+            downloadContainer = (FrameLayout) view.findViewById(R.id.fl_container);
+            progressBar = new ProgressBar(view.getContext());
 
             playButton.setTag(R.drawable.play);
 
             final Resources resources = view.getResources();
+
+            progressBar.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    // Cancel the download
+
+                    // Replace the progress spinner with the download button
+                    downloadContainer.removeAllViews();
+                    downloadContainer.addView(downloadButton);
+                }
+            });
 
             playButton.setOnClickListener(new View.OnClickListener() {
                 @Override
